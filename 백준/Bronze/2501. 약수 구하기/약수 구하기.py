9b79f6b356c251a44883b4021bec44c1af4fe7fe
ssy330 <gimsoyeon092@gmail.com>
2024-09-20 22:06:43 +0900

    [Bronze III] Title: 약수 구하기, Time: 76 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+count = 0
+i = 0
+for i in range(1, N+1):
+    if N % i == 0:
+        count += 1
+
+    if count == K:
+        print(i)
+        break
+
+if count < K:
+    print(0)
